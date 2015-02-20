c77b8b31b66e5abb26bb97099678530d40fd9130
Aaron Rosenfeld <ar374@drexel.edu>
2015-02-20 14:23:43 -0500

    adding and searching works

--- src/main.go
+++ src/main.go
@@ -1,41 +1,92 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "os"
     "os/user"
+    "strconv"
+    "strings"
     "path/filepath"
 
     "gopkg.in/alecthomas/kingpin.v1"
 )
 
-func add(pid int, note string) {
+type ProcNotes map[int]string
+
+func addNote(notes ProcNotes, pid int, note string) {
+    notes[pid] = note
 }
 
-func save(notes map[int]string, file *os.File) {
+func printNotes(notes ProcNotes) {
+    fmt.Printf("PID\tNOTE\n")
+    for pid, note := range notes {
+        fmt.Printf("%d\t%s\n", pid, note)
+    }
+}
+
+func searchNotes(notes ProcNotes, query string) ProcNotes {
+    query = strings.ToLower(query)
+    found := make(ProcNotes)
+
+    for pid, note := range notes {
+        if strings.Index(strings.ToLower(note), query) >= 0 {
+            found[pid] = note
+        }
+    }
+
+    return found
+}
+
+func saveNoteFile(notes ProcNotes, file *os.File) {
+    file.Truncate(0)
+    file.Seek(0, 0)
+    for pid, note := range notes {
+        _, err := file.WriteString(fmt.Sprintf("%d %s\n", pid, note))
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+    file.Sync()
 }
 
 func makeNoteFile(path string) (*os.File, error) {
     if _, err := os.Stat(path); os.IsNotExist(err) {
-        _, err := os.Create(path)
+        fh, err := os.Create(path)
         if err != nil {
             return nil, err
         }
+        return fh, nil
     }
 
-    return os.Open(path)
+    return os.OpenFile(path, os.O_RDWR, os.FileMode(0666))
 }
 
-func readNotes(file *os.File) (map[int]string, error) {
-    notes := make(map[int]string)
+func readNoteFile(file *os.File) (ProcNotes, error) {
+    notes := make(ProcNotes)
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        entry := strings.SplitN(strings.TrimSpace(scanner.Text()), " ", 2)
+        pid, err := strconv.Atoi(entry[0])
+        if err != nil {
+            fmt.Println("Unable to read note entry: ", entry, entry[0])
+            continue
+        }
+        notes[pid] = entry[1]
+    }
     return notes, nil
 }
 
 func main() {
     var (
-        action = kingpin.Command("add", "Adds a note to a process.")
-        pid = action.Arg("pid", "Process ID.").Required().Int()
-        note = action.Arg("note", "Note for process.").Required().String()
+        addAction = kingpin.Command("add", "Adds a note to a process.")
+        pid = addAction.Arg("pid", "Process ID.").Required().Int()
+        note = addAction.Arg("note", "Note for process.").Required().String()
+
+        _ = kingpin.Command("list", "Lists all process notes.")
+        searchAction = kingpin.Command("search", "Searches for a process matching the query.")
+        query = searchAction.Arg("query", "The query to run.").Required().String()
     )
 
     usr, err := user.Current()
@@ -45,12 +96,14 @@ func main() {
     }
 
     noteFile, err := makeNoteFile(filepath.Join(usr.HomeDir, ".procnote"))
+    defer noteFile.Close()
+
     if err != nil {
         fmt.Println("Unable to create or open note file:", err)
         return
     }
 
-    notes, err := readNotes(noteFile)
+    notes, err := readNoteFile(noteFile)
     if err != nil {
         fmt.Println("Unable to read note file:", err)
         return
@@ -58,9 +111,14 @@ func main() {
 
     switch kingpin.Parse() {
         case "add":
-            add(*pid, *note)
+            addNote(notes, *pid, *note)
+            saveNoteFile(notes, noteFile)
+            break
+        case "list":
+            printNotes(notes)
+            break
+        case "search":
+            printNotes(searchNotes(notes, *query))
             break
     }
-
-    save(notes, noteFile)
 }
